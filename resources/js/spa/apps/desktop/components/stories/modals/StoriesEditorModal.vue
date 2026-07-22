f8631cddf3ddea11cba95e1bf3bdc1fa420d0497
ANTAX <admin@antax.id>
2026-07-22 17:00:12 +0700

    init: ANTAX rombak untuk Railway deployment

---
+++ resources/js/spa/apps/desktop/components/stories/modals/StoriesEditorModal.vue
@@ -0,0 +1,22 @@
+<template>
+	<Backdrop>
+		<div class="ml-navbar py-top-offset">
+			<StoriesEditor></StoriesEditor>
+		</div>
+	</Backdrop>
+</template>
+
+<script>
+	import { defineComponent } from 'vue';
+	import ANTAXAttribution from '@D/components/attributions/ANTAXAttribution.vue';
+    import Backdrop from '@D/components/general/modals/Backdrop.vue';
+	import StoriesEditor from '@D/components/stories/editor/StoriesEditor.vue';
+
+	export default defineComponent({
+		components: {
+			StoriesEditor: StoriesEditor,
+			ANTAXAttribution: ANTAXAttribution,
+			Backdrop: Backdrop
+		}
+	});
+</script>
